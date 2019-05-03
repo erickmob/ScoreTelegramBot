499f670cf9dc943ff66e1e6b0c4dbb9b7df84fab
erick.oliveira <erick.oliveira@projuris.com.br>
2019-05-03 16:12:44 -0300

    telegram bot configs

---
+++ src/main/java/com/erick/telegram/slavebot/domain/Bot.java
@@ -0,0 +1,58 @@
+package com.erick.telegram.slavebot.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import javax.annotation.PostConstruct;
+
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    private static final Logger logger = LoggerFactory.getLogger(Bot.class);
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(chatId);
+            String text = message.getText();
+            response.setText(text);
+            try {
+                execute(response);
+                logger.info("Sent message \"{}\" to {}", text, chatId);
+            } catch (TelegramApiException e) {
+                logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+            }
+        }
+    }
+
+    @PostConstruct
+    public void start() {
+        logger.info("username: {}, token: {}", username, token);
+    }
+}
