03dd409d3731ef913136a0d244e0d2de3a02fbb4
erick.oliveira <erick.oliveira@projuris.com.br>
2019-05-03 13:47:34 -0300

    first

---
+++ src/main/java/com/erickmob/telegram/slavebot/SlavebotApplication.java
@@ -0,0 +1,13 @@
+package com.erickmob.telegram.slavebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlavebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlavebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erickmob/telegram/slavebot/SlavebotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.erickmob.telegram.slavebot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlavebotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
